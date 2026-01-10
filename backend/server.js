ea94ad283f8e28fcfdac1c76dd452750b7f161df
Satyam Rana <satyamrana@192.168.1.46>
2026-01-10 20:54:36 +0530

    Cors

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://workmanager-frontend.onrender.com"],
   credentials: true
 }));
 app.use(express.json());
